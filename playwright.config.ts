de102e7872b856776530ca7eb0129b39f44023ba
Sam <39275315+degrootsam@users.noreply.github.com>
2025-09-30 15:25:47 +0200

    feat(trace report): enabled trace reports
    WThis allows SAMURAI to display additional information next to the steps that was executed

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,11 +25,13 @@ export default defineConfig({
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: process.env.APP_URL || "",
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    screenshot: { mode: "off" },
+    trace: {
+      screenshots: true,
+      mode: "on",
+      snapshots: false,
+      attachments: false,
+    },
   },
 
   /* Configure projects for major browsers */
@@ -50,7 +52,7 @@ export default defineConfig({
       testDir: "src/admin/tests",
       metadata: { type: "suite" },
     },
-  ]
+  ],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
